4eb62e1499194da6329ebceb5a5b0daa588cfc58
Khoi Viet <vietvk@antsprogrammatic.com>
2020-01-13 13:51:02 +0700

    + Commit first version.

---
+++ InsightSDK.framework/Headers/InsightSDK.h
@@ -0,0 +1,19 @@
+//
+//  InsightSDK.h
+//  InsightSDK
+//
+//  Created by KhoiViet on 12/19/19.
+//  Copyright © 2019 KhoiViet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InsightSDK.
+FOUNDATION_EXPORT double InsightSDKVersionNumber;
+
+//! Project version string for InsightSDK.
+FOUNDATION_EXPORT const unsigned char InsightSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InsightSDK/PublicHeader.h>
+
+
